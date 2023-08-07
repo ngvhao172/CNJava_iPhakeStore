43910788c2f73a6b9d77d8bb9931cae3a18244be
Nguyễn Hào <ngvhao172@gmail.com>
2023-08-07 15:04:44 +0700

    up

---
+++ target/classes/static/main.js
@@ -0,0 +1,8 @@
+
+function formatCurrency(amount) {
+    const formatter = new Intl.NumberFormat('vi-VN', {
+        style: 'currency',
+        currency: 'VND',
+    });
+    return formatter.format(amount);
+}
